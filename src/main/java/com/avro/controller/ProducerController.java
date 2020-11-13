a448c7ccf67fb3f4a39d1d36311217caae9a141a
Greti Cani <greti.cani@moveengineering-eu.com>
2020-11-13 15:51:24 +0100

    second

--- src/main/java/com/avro/controller/ProducerController.java
+++ src/main/java/com/avro/controller/ProducerController.java
@@ -1,8 +1,10 @@
 package com.avro.controller;
 
 import com.avro.Customer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -17,11 +19,21 @@ public class ProducerController implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        for (int i = 0 ; i<10; i++){
+        for (int i = 10 ; i<3000; i++){
             Customer customer = Customer.newBuilder().setFirstName("Customer_"+i).setLastName("lastname_"+i).setAge(25+i).setHeight(180+i).setWeight(70+i).build();
             template.send("customer_topic",customer);
         }
 
 
     }
+
+
+    @KafkaListener(topics = "customer_topic")
+    public void print(ConsumerRecord<String, Customer> record){
+        System.err.println(record.value());
+    }
+
+
+
+
 }
